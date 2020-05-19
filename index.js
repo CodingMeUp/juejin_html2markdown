8fea0c1f909b05631319aebd8d25550ece79361e
cyn <363150761@qq.com>
2020-05-19 23:42:54 +0800

    使用html-markdown替换turndown，并对图片进行正则替换兼容

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var TurndownService = require('turndown');
-var tService = new TurndownService()
+// var TurndownService = require('turndown');
+var html2md = require('html-markdown');
+// var tService = new TurndownService();
 var request = require('request');
 var cheerio = require('cheerio');
 var fs=require('fs');
@@ -10,11 +11,16 @@ if (process.argv && process.argv.length > 2) {
     if (!error && response.statusCode == 200) {
       // 输出网页内容
       const $ = cheerio.load(body);
-      const art = $('html').find('article').html();
+      let art = $('html').find('article').html();
       const title = $('html').find('.article-title').text();
-      const markdown = tService.turndown(art);
+      // const markdown = tService.turndown(art);
+      // 替换data-src -> src
+      const regex = /data-src/ig;
+      art = art.replace(regex, 'src');
+      const md1 = html2md.html2mdFromString(art);
+      // use markdown or md1
       //3. fs.writeFile  写入文件（会覆盖之前的内容）（文件不存在就创建）  utf8参数可以省略
-      fs.writeFile(title.substring(0, 15) + '.md', markdown, 'utf8', function (error) {
+      fs.writeFile(title.substring(0, 15) + '.md', md1, 'utf8', function (error) {
         if (error) {
           console.log(error);
           return false;
@@ -27,4 +33,3 @@ if (process.argv && process.argv.length > 2) {
     console.log(error);
     return false;
 }
-
